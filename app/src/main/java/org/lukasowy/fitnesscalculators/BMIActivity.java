7636717ee0e4b1e123bafaff74a06f742a6f7f2e
Lukasowy <lukasowy@gmail.com>
2017-11-20 20:17:39 +0100

    Set fonts for all activity

--- app/src/main/java/org/lukasowy/fitnesscalculators/BMIActivity.java
+++ app/src/main/java/org/lukasowy/fitnesscalculators/BMIActivity.java
@@ -1,13 +1,22 @@
 package org.lukasowy.fitnesscalculators;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class BMIActivity extends AppCompatActivity {
+
+    TextView titleViewBMI;
+    Typeface titleFont;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi);
+
+        titleViewBMI = (TextView) findViewById(R.id.titleViewBMI);
+        titleFont = Typeface.createFromAsset(getAssets(), "LobsterTwo-Italic.otf");
+        titleViewBMI.setTypeface(titleFont);
     }
 }
--- app/src/main/java/org/lukasowy/fitnesscalculators/MainActivity.java
+++ app/src/main/java/org/lukasowy/fitnesscalculators/MainActivity.java
@@ -1,21 +1,31 @@
 package org.lukasowy.fitnesscalculators;
+
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     GridLayout mainGrid;
+    LinearLayout allScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mainGrid = (GridLayout) findViewById(R.id.mainGrid);
+        allScreen = (LinearLayout) findViewById(R.id.allScreen);
+
+        //Set fonts for all activity
+        Typeface textFont = Typeface.createFromAsset(this.getAssets(), "LobsterTwo-Italic.otf");
+        setFontToAllChilds(allScreen, textFont);
 
         //Set Event
         setSingleEvent(mainGrid);
@@ -29,15 +39,15 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View view) {
                     Intent intent = null;
-                    if(cardView == mainGrid.getChildAt(0)) {
+                    if (cardView == mainGrid.getChildAt(0)) {
                         intent = new Intent(MainActivity.this, BMIActivity.class);
-                    } else if(cardView == mainGrid.getChildAt(1)) {
+                    } else if (cardView == mainGrid.getChildAt(1)) {
                         intent = new Intent(MainActivity.this, WaterActivity.class);
-                    } else if(cardView == mainGrid.getChildAt(2)) {
+                    } else if (cardView == mainGrid.getChildAt(2)) {
                         intent = new Intent(MainActivity.this, MaxHeartRateActivity.class);
-                    } else if(cardView == mainGrid.getChildAt(3)) {
+                    } else if (cardView == mainGrid.getChildAt(3)) {
                         intent = new Intent(MainActivity.this, MetabolicActivity.class);
-                    } else if(cardView == mainGrid.getChildAt(4)) {
+                    } else if (cardView == mainGrid.getChildAt(4)) {
                         intent = new Intent(MainActivity.this, ProteinActivity.class);
                     } else {
                         intent = new Intent(MainActivity.this, CreatineActivity.class);
@@ -45,6 +55,19 @@ public class MainActivity extends AppCompatActivity {
                     view.getContext().startActivity(intent);
                 }
             });
+        }
+    }
+
+    //Set fonts for all activity
+    private void setFontToAllChilds(ViewGroup myMostParentLayout, Typeface tf) {
+        int childCount = myMostParentLayout.getChildCount();
+        for (int i = 0; i < childCount; ++i) {
+            View child = myMostParentLayout.getChildAt(i);
+
+            if (child instanceof ViewGroup)
+                setFontToAllChilds((ViewGroup) child, tf);
+            else if (child instanceof TextView)
+                ((TextView) child).setTypeface(tf);
         }
     }
 }
