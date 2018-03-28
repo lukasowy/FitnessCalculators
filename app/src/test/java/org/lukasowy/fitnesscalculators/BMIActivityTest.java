28d5049074a0b58efcbbf9d0d8cb48bd8ffb4452
Lukasowy <lukasowy@gmail.com>
2018-03-28 07:39:02 +0200

    Added automation test in Espresso

--- app/src/test/java/org/lukasowy/fitnesscalculators/BMIActivityTest.java
+++ app/src/test/java/org/lukasowy/fitnesscalculators/BMIActivityTest.java
@@ -21,6 +21,7 @@ public class BMIActivityTest {
 
     @Before
     public void before() {
+
         bmiActivity = new BMIActivity();
     }
 
--- app/src/test/java/org/lukasowy/fitnesscalculators/ExampleUnitTest.java
+++ app/src/test/java/org/lukasowy/fitnesscalculators/ExampleUnitTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    BMIActivityTest test = new BMIActivityTest();
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
